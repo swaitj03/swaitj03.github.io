df77882214c36940e186f985eaa7a6a68e74c3e9
jade Swait <jadeswait123@gmail.com>
2025-03-03 15:09:45 +0200

    Added Lab 3 javascript

---
+++ 2597769-lab3.js
@@ -0,0 +1,49 @@
+const tracks = [
+    { title: 'Blinding Lights', artist: 'The Weeknd', year: 2020 },
+    { title: 'Levitating', artist: 'Dua Lipa', year: 2021 },
+    { title: 'Save Your Tears', artist: 'The Weeknd', year: 2020 },
+  ];
+
+function getMusicTitlesByYear(music){
+    if(!Array.isArray(music)){
+        throw new TypeError("Not an array");
+    }
+
+    if(music.length == 0){
+        return {};
+    }
+
+    let musicByYear = {};
+    music.forEach(element => {
+        if(element.year == undefined){
+            throw new TypeError("Year is undefined");
+        }
+        if(typeof element.year != "number"){
+            throw new TypeError("Year is not a string");
+        }
+
+        if(element.title == undefined){
+            throw new TypeError("Title is undefined");
+        }
+        if(typeof element.title != "string"){
+            throw new TypeError("Title is not a string");
+        }
+
+        if(element.artist == undefined){
+            throw new TypeError("Artist is undefined");
+        }
+        if(typeof element.artist != "string"){
+            throw new TypeError("Artist is not a string");
+        }
+
+        if(musicByYear[element.year] == undefined){
+            musicByYear[element.year] = []
+        }
+        musicByYear[element.year].push(element.title);
+        musicByYear[element.year].sort();
+    });
+
+    return musicByYear
+}
+
+console.log(getMusicTitlesByYear(tracks));
